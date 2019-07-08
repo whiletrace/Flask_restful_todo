c6036361527076176c078a4079fca0daa684b895
Trace Harris <traceh@whiletrace.com>
2019-07-07 22:41:09 -0700

    hotfix test_database tests are passing in local but in ci

--- todo/models.py
+++ todo/models.py
@@ -1,16 +1,18 @@
 from peewee import *
 from playhouse.flask_utils import FlaskDB
 
-
+"""
 class NewFlaskDb(FlaskDB):
-    """ subclass of FlaskDB as workaround for db connection leakage"""
+    subclass of FlaskDB as workaround for db connection leakage
     def connect_db(self):
-        """overridden method now reuses db connection if already open"""
+        overridden method now reuses db connection if already open
         self.database.connect(reuse_if_open=True)
+"""
+
 
 
 #  instantiation of Database proxy obj
-db_wrapper = NewFlaskDb()
+db_wrapper = FlaskDB()
 
 
 class Todo(db_wrapper.Model):
